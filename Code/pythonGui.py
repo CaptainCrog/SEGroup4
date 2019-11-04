a8e9be39d7e5c93948c27e9c0d3622a16b4a5121
rharris24 <rob.harris1998@gmail.com>
2019-11-04 14:35:54 +0000

    GUI changes
    Added checkbox and changed colour scheme

--- Code/pythonGui.py
+++ Code/pythonGui.py
@@ -1,15 +1,29 @@
-#!/usr/bin/python
-
-import tkinter
+from tkinter import *
 import tkinter.messagebox
+import tkinter
 
 top = tkinter.Tk()
-top.geometry('900x480')
-top.title("Spotify New Application")
-def helloCallBack():
-   tkinter.messagebox.showinfo( "Hello Python", "Hello World")
+top.geometry('480x350')
+top.configure(bg = "#191414")
+top.title("News based Spotify Suggestions")
 
-B = tkinter.Button(top, text ="Get News", command = helloCallBack)
-B.pack(padx = 50, pady = 50)
+def helloCallBack():
+   tkinter.messagebox.showinfo("Hello", "Hello 2: Electric Boogaloo")
+
+def checkBoxCallBack():
+   print ("Checkbox checked")
+   #adds checkbox option to query?
+
+
+#canvas = Canvas(top, width = 300, height = 300)      
+#canvas.pack() 
+#img = PhotoImage(file = "Spotify_Logo.ppm")
+#canvas.create_image(50, 50, image = img)
+   
+B2 = tkinter.Checkbutton(top, text = "Politics", command = checkBoxCallBack, bg = "#1DB954")
+B2.place(x = 50, y = 50)
+
+B = tkinter.Button(top, text ="Get News", command = helloCallBack, bg = "#1DB954")
+B.place(bordermode=OUTSIDE, height = 50, width = 100,  x = 190, y = 250)
 # Code to add widgets will go here...
 top.mainloop()
